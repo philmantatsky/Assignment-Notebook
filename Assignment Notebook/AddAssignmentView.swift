87c2570269473a2eb2ac59a7716c14e30dd3d56a
Phillip Mantatsky <pmantatsky1@bsd220.org>
2024-02-08 09:52:15 -0600

    added addAssignmentView

--- Assignment Notebook/AddAssignmentView.swift
+++ Assignment Notebook/AddAssignmentView.swift
@@ -2,7 +2,33 @@
 //  AddAssignmentView.swift
 //  Assignment Notebook
 //
-//  Created by Phillip Mantatsky on 2/5/24.
+//  Created by Phillip Mantatsky on 2/8/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct AddAssignmentView: View {
+    @ObservedObject var assignmentList : AssignmentList
+    @State private var course = ""
+    @State private var description = ""
+    @State private var dueDate = Date()
+    @Environment(\.presentationMode) var presentationMode
+    static let courses = ["High", "Medium", "Low"]
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker("course", selection: $course) {
+                    ForEach(Self.courses, id: \.self) { course in
+                        Text(course)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct AddAssignmentView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddAssignmentView(assignmentList: AssignmentList())
+    }
+}
